88de1492ab784a5656adc3d432142d5b20874f16
mao-tk <126842098+mao-tk@users.noreply.github.com>
2023-04-28 04:20:20 +0000

    [Add]アニメーション

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
-$(document).ready(function () {
-  $('body').html('<h1>Hello jQuery!!</h1>');
+/*global $*/
+$(function(){
+  $('.box1').slideDown(function(){
+    $('.box1').css({
+      'background-color': 'blue',
+      'width': '200px',
+      'height': '100px'
+    }).slideUp();
+  });
 });
